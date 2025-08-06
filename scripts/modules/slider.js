9e461f803a993e4e3a15e8157bab8ac431393bd5
Crystyan Verly <crystyanknappverly@gmail.com>
2025-08-06 16:38:18 -0300

    organize class by recommended structure

--- scripts/modules/slider.js
+++ scripts/modules/slider.js
@@ -1,4 +1,5 @@
 import throttle from './throttle.js';
+
 export default class Slider {
   constructor(wrapper, rail, config = {}) {
     this.wrapper = document.querySelector(wrapper);
@@ -24,12 +25,41 @@ export default class Slider {
     this.itemsOnRail();
     this.bindingMethods();
   }
+
+  // ==== PUBLIC ====
+  init() {
+    if (this.wrapper && this.rail) {
+      this.addStartEvent();
+      this.changeItemTo(this.config.initialItem);
+    } else {
+      console.warn(`No wrapper or rail found.`);
+    }
+  }
+
+  // ==== SETUP ====
   bindingMethods() {
     const methodsToBind = ['onStart', 'onFinal', 'prevItem', 'nextItem'];
-    methodsToBind.forEach((method) => (this[method] = this[method].bind(this)));
+    methodsToBind.forEach((method) => {
+      this[method] = this[method].bind(this);
+    });
 
     this.onMoving = throttle(this.onMoving.bind(this), 16);
   }
+
+  addStartEvent() {
+    this.wrapper.addEventListener('pointerdown', this.onStart, {
+      passive: false,
+    });
+
+    const controlEvent = ['click', 'touchstart'];
+    if (this.config.prevControl && this.config.nextControl) {
+      controlEvent.forEach((event) => {
+        this.config.prevControl.addEventListener(event, this.prevItem);
+        this.config.nextControl.addEventListener(event, this.nextItem);
+      });
+    }
+  }
+
   itemsOnRail() {
     this.arrItems = [...this.rail.children].map((item) => {
       const onLeft = -item.offsetLeft;
@@ -37,17 +67,33 @@ export default class Slider {
     });
     return this.arrItems;
   }
+
+  // ==== MOVIMENT ====
   updatePosition(currentX) {
     const calcDist = Math.round((currentX - this.distances.initial) * 1.6);
     this.distances.moving = calcDist;
     return this.distances.final + calcDist;
   }
+
   moveSlide(distX, transition = true) {
     this.rail.style.transform = `translate3d(${distX}px, 0px, 0px)`;
-    transition
-      ? (this.rail.style.transition = `transform .3s ease-in-out`)
-      : (this.rail.style.transition = 'none');
+    this.rail.style.transition = transition
+      ? 'transform .3s ease-in-out'
+      : 'none';
   }
+
+  changeItemTo(index) {
+    if (index < this.arrItems.length) {
+      const { onLeft } = this.arrItems[index];
+      this.moveSlide(onLeft);
+      this.distances.final = onLeft;
+      this.index = this.directionLogic(index);
+    } else {
+      console.warn('Index is bigger than array length');
+    }
+  }
+
+  // ==== DIRECTION LOGIC ====
   directionLogic(index) {
     const lastItem = this.arrItems.length - 1;
     const isInfinit = this.config.isInfinit;
@@ -57,33 +103,31 @@ export default class Slider {
 
     return (this.index = { prev, active: index, next });
   }
-  changeItemTo(index) {
-    if (index < this.arrItems.length) {
-      const { onLeft } = this.arrItems[index];
-      this.moveSlide(onLeft);
-      this.distances.final = onLeft;
-      this.index = this.directionLogic(index);
-    } else console.warn('Index is bigger than array length');
-  }
+
   prevItem(e) {
     e.preventDefault();
     if (this.index.prev !== undefined) {
       this.changeItemTo(this.index.prev);
     }
   }
+
   nextItem(e) {
     e.preventDefault();
     if (this.index.next !== undefined) {
       this.changeItemTo(this.index.next);
     }
   }
-  changeOnMoving(e) {
-    const minMove = this.wrapper.offsetWidth * 0.07;
-    if (this.distances.moving < -minMove && this.index.next !== undefined)
-      this.nextItem(e);
-    else if (this.distances.moving > minMove && this.index.prev !== undefined)
-      this.prevItem(e);
-    else this.changeItemTo(this.index.active);
+
+  // ==== EVENTS (DRAG) ====
+  onStart(e) {
+    e.preventDefault();
+    this.distances.initial = Math.round(e.clientX);
+    this.wrapper.addEventListener('pointermove', this.onMoving);
+    this.wrapper.addEventListener('pointerup', this.onFinal);
+  }
+
+  onMoving(e) {
+    this.moveSlide(this.updatePosition(e.clientX), false);
   }
 
   onFinal(e) {
@@ -92,31 +136,19 @@ export default class Slider {
     this.changeOnMoving(e);
     this.distances.moving = 0;
   }
-  onMoving(e) {
-    this.moveSlide(this.updatePosition(e.clientX), false);
-  }
-  onStart(e) {
-    e.preventDefault();
-    this.distances.initial = Math.round(e.clientX);
-    this.wrapper.addEventListener('pointermove', this.onMoving);
-    this.wrapper.addEventListener('pointerup', this.onFinal);
-  }
-  addStartEvent() {
-    this.wrapper.addEventListener('pointerdown', this.onStart, {
-      passive: false,
-    });
-    const controlEvent = ['click', 'touchstart'];
-    if (this.config.prevControl && this.config.nextControl)
-      controlEvent.forEach((event) => {
-        this.config.prevControl.addEventListener(event, this.prevItem);
-        this.config.nextControl.addEventListener(event, this.nextItem);
-      });
-  }
 
-  init() {
-    if (this.wrapper && this.rail) {
-      this.addStartEvent();
-      this.changeItemTo(this.config.initialItem);
-    } else console.warn(`No wrapper or rail found.`);
+  changeOnMoving(e) {
+    const minMove = this.wrapper.offsetWidth * 0.07;
+
+    if (this.distances.moving < -minMove && this.index.next !== undefined) {
+      this.nextItem(e);
+    } else if (
+      this.distances.moving > minMove &&
+      this.index.prev !== undefined
+    ) {
+      this.prevItem(e);
+    } else {
+      this.changeItemTo(this.index.active);
+    }
   }
 }
